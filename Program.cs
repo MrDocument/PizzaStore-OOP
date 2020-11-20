2249dcd87a81f668a08eb4d4be0c94d6f1b6638e
MrDocument <64020746+MrDocument@users.noreply.github.com>
2020-11-20 18:37:56 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using PizzaStore_1.Store;
+using System;
+
+namespace PizzaStore_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var huePizzaStore = new HUEPizzaStore();
+            huePizzaStore.OrderPizza("cheese");
+            //var dnPizzaStore = new DNPizzaStore();
+            //dnPizzaStore.OrderPizza("cheese");
+            //var hcmPizzaStore = new HCMPizzaStore();
+            //hcmPizzaStore.OrderPizza("cheese");
+
+
+            Console.ReadLine();
+        }
+    }
+}
